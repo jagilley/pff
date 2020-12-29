50dcd00be05f72b0a8d0bb575c44ee391f8641c7
nandary99 <57537994+nandary99@users.noreply.github.com>
2020-12-29 17:05:25 -0500

    Function getoffensedata added to search by player

--- main.py
+++ main.py
@@ -15,43 +15,49 @@ from bs4 import BeautifulSoup
 #url_template = "https://www.pro-football-reference.com/years/{year}/games.htm"
 
 # Iterate Player Data Frame for Each Year Specified
-
-nfl_df = pd.DataFrame()
-
-
-url = "https://www.pro-football-reference.com/boxscores/"
-
-html = urlopen(url).read()
-
-soup = BeautifulSoup(html)
+def getoffensedata(player):
+    nfl_df = pd.DataFrame()
 
 
-tagz= soup.findAll('td', {"class": "right gamelink"})
-linx=[]
-for tag in tagz:
-    linxtag=tag.findAll('a')
-    linxtag="https://www.pro-football-reference.com" + linxtag[0].attrs['href']
-    linx.append(linxtag)
-
-column_headers = ['Player', 'Tm', 'Cmp', 'Att', 'Yds', 'TD', 'Int', 'Sk', 'Yds', 'Lng', 'Rate', 'Att', 'Yds', 'TD', 'Lng', 'Tgt', 'Rec', 'Yds', 'TD', 'Lng', 'Fmb', 'FL']
-statdataframe= pd.DataFrame(columns=column_headers)
-
-for a in linx:
-    url = a
+    url = "https://www.pro-football-reference.com/boxscores/"
 
     html = urlopen(url).read()
 
     soup = BeautifulSoup(html)
-    stattables = soup.findAll('div', {"id": "all_player_offense"})
-    # look= soup.findAll('thead')
-    headz = stattables[0].findAll('tr')[1]
-    column_headers = [th.getText() for th in stattables[0].findAll('tr')[1].findAll('th')]
 
-    data_rows = stattables[0].findAll('tbody', limit=1)[0].findAll('tr', {"class": None})[0:]
 
-    player_data = [[td.getText() for td in data_rows[i].findAll(['th', 'td'])]
-                   for i in range(len(data_rows))]
-    fakedf=pd.DataFrame(player_data, columns=column_headers)
-    statdataframe = pd.concat([statdataframe, fakedf])
-print("poopyfarts")
+    tagz= soup.findAll('td', {"class": "right gamelink"})
+    linx=[]
+    for tag in tagz:
+        linxtag=tag.findAll('a')
+        linxtag="https://www.pro-football-reference.com" + linxtag[0].attrs['href']
+        linx.append(linxtag)
+
+    column_headers = ['Player', 'Tm', 'Cmp', 'PassAtt', 'PassYds', 'PassTD', 'Int', 'Sk', 'SkYds', 'PassLng', 'Rate', 'RushAtt', 'RushYds', 'RushTD', 'RushLng', 'Tgt', 'Rec', 'RecYds', 'RecTD', 'RecLng', 'Fmb', 'FL']
+    statdataframe= pd.DataFrame(columns=column_headers)
+
+    for a in linx:
+        url = a
+
+        html = urlopen(url).read()
+
+        soup = BeautifulSoup(html)
+        stattables = soup.findAll('div', {"id": "all_player_offense"})
+        if not stattables:
+            return statdataframe
+
+        # look= soup.findAll('thead')
+        headz = stattables[0].findAll('tr')[1]
+        #column_headers = [th.getText() for th in stattables[0].findAll('tr')[1].findAll('th')]
+
+        data_rows = stattables[0].findAll('tbody', limit=1)[0].findAll('tr', {"class": None})[0:]
+
+        player_data = [[td.getText() for td in data_rows[i].findAll(['th', 'td'])]
+                       for i in range(len(data_rows))]
+        fakedf=pd.DataFrame(player_data, columns=column_headers)
+        statdataframe = pd.concat([statdataframe, fakedf])
+    retval= statdataframe[statdataframe['Player'].str.contains(player)]
+    return retval
+getoffensedata("Kirk Cousins")
+print("f")
 
